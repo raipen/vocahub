b61d1b1766e43d26a29ce99dec1070be6b6db56e
raipen <raipen@knu.ac.kr>
2024-02-27 01:24:38 +0900

    feat: delete unused import

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,4 @@
-import { Prisma,PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { NotFoundError } from '@errors';
 import * as User from '@DTO/user.dto';
 import crypto from 'crypto';
