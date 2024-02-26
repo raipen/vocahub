318978882c97cdd99d24f8ab5a22782f909f7703
raipen <raipen@knu.ac.kr>
2024-02-26 20:49:37 +0900

    feat: auth apis

---
+++ src/api/routes/user.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance, FastifyPluginAsync } from 'fastify';
+import onError from '@hooks/onError';
+import checkUser from '@hooks/checkUser';
+import checkRefreshToken from '@hooks/checkRefreshToken';
+import * as User from '@DTO/user.dto';
+import userService from '@services/userService';
+
+const api: FastifyPluginAsync = async (server: FastifyInstance) => {
+    server.post<User.signUpInterface>(
+        '/signUp',
+        {
+            schema: User.signUpSchema,
+            onError
+        },
+        async (request, reply) => {
+            const { accessToken, refreshToken } = await userService.signUp(request.body);
+            reply.status(201).setCookie('authorization', refreshToken).send({ accessToken });
+        }
+    );
+    server.post<User.signInInterface>(
+        '/signIn',
+        {
+            schema: User.signInSchema,
+            onError
+        },
+        async (request, reply) => {
+            const { accessToken, refreshToken } = await userService.signIn(request.body);
+            reply.setCookie('authorization', refreshToken).send({ accessToken });
+        }
+    );
+    server.post(
+        '/signOut',
+        {
+            schema: User.signOutSchema,
+            onError
+        },
+        async (request, reply) => {
+            reply.clearCookie('authorization').send();
+        }
+    );
+    server.post(
+        '/refresh',
+        {
+            schema: User.refreshSchema,
+            preValidation: checkRefreshToken,
+            onError
+        },
+        async (request, reply) => {
+            const { accessToken } = await userService.refresh(request.body);
+            reply.send({ accessToken });
+        }
+    );
+};
+
+export default api;
---
+++ src/services/userService.ts
@@ -0,0 +1,57 @@
+import { Prisma,PrismaClient } from '@prisma/client';
+import { NotFoundError } from '@errors';
+import * as User from '@DTO/user.dto';
+import crypto from 'crypto';
+import { LoginToken } from '@utils/jwt';
+const prisma = new PrismaClient();
+
+export default {
+    async signUp({name,password}: User.signUpInterface['Body']){
+        const salt = crypto.randomBytes(64).toString('hex');
+        const hashedPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+        const result = await prisma.user.create({
+            data: {
+                name,
+                password: hashedPassword,
+                salt
+            }
+        });
+        const loginToken = new LoginToken(result.uuid);
+        const accessToken = loginToken.signAccessToken();
+        const refreshToken = loginToken.signRefreshToken();
+        return {
+            accessToken,
+            refreshToken
+        };
+    },
+
+    async signIn({name,password}: User.signInInterface['Body']){
+        const user = await prisma.user.findUnique({
+            where: {
+                name
+            }
+        });
+        if (!user) {
+            throw new NotFoundError('User not found');
+        }
+        const hashedPassword = crypto.pbkdf2Sync(password, user.salt, 10000, 64, 'sha512').toString('hex');
+        if (hashedPassword !== user.password) {
+            throw new NotFoundError('Password not match');
+        }
+        const loginToken = new LoginToken(user.uuid);
+        const accessToken = loginToken.signAccessToken();
+        const refreshToken = loginToken.signRefreshToken();
+        return {
+            accessToken,
+            refreshToken
+        };
+    },
+
+    async refresh({userId}: User.refreshInterface['Body']){
+        const loginToken = new LoginToken(userId);
+        const accessToken = loginToken.signAccessToken();
+        return {
+            accessToken
+        };
+    }
+}
