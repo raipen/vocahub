1b0e669d4ba87d4690eb0d482d58495bce492238
raipen <raipen@knu.ac.kr>
2024-02-27 11:48:11 +0900

    feat: add getwordbook api

--- src/repository/wordbook.repo.ts
+++ src/repository/wordbook.repo.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
+export const getWordbook = async (wordbookId: number, userId: string) => await prisma.wordbook.findFirst({
+    where: {
+        id: wordbookId,
+        userId
+    }
+});
+
+
 const getWordbookList = (isHidden:boolean) => async (userId: string) => await prisma.wordbook.findMany({
     where: {
         userId,
