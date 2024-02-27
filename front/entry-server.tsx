ad63baad9772cce6954a45b9e08fa1badc4332f0
raipen <raipen@knu.ac.kr>
2024-02-27 22:35:13 +0900

    feat: rename front entry file

---
+++ front/entry-server.tsx
@@ -0,0 +1,9 @@
+import ReactDOMServer from 'react-dom/server'
+import App from './App';
+
+export function render() {
+  const html = ReactDOMServer.renderToString(
+      <App />
+  )
+  return { html }
+}
