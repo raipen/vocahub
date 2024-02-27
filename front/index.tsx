fce2ce863d1517ce3aebf85de097dd73e5d4657d
raipen <raipen@knu.ac.kr>
2024-02-27 21:45:40 +0900

    feat: move front files

---
+++ front/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+    <App />
+);
