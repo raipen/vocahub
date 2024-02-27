b07ff7e8fb1e04b05ef65b08e0d0bfde6bc26bbf
raipen <raipen@knu.ac.kr>
2024-02-27 15:28:34 +0900

    error: connect root dto to front

--- front/src/utils/apis/auth.ts
+++ front/src/utils/apis/auth.ts
@@ -1,11 +1,23 @@
+import axios from 'axios';
+import * as User from '@DTO/user.dto';
+import { ErrorInterface } from '@DTO/index.dto';
+import { ErrorWithToast } from '@errors/index';
 type login = {
   name: string;
   password: string;
 }
 
 export const requestLogin = async ({name, password}: login) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return "access_token";
+  try{
+    const response = await axios.post<User.signInInterface['Reply']['200']>('/api/v1/user/signIn', {name, password});
+    return response.data.accessToken;
+  }catch(e: unknown){
+    if(!axios.isAxiosError(e)||!e.response){
+      throw e;
+    }
+    const data = e.response.data as ErrorInterface;
+    throw new ErrorWithToast(data.message);
+  }
 }
 
 export const requestRefresh = async () => {
