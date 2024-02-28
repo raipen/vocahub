8dbf8f4cdefc4b025623ebc2def3b213fdecfc10
raipen <raipendalk@gmail.com>
2024-02-28 21:30:35 +0900

    feat: load api routers

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance, FastifyPluginAsync, FastifySchema } from 'fastify';
 import test from './routes/apiTest';
 import user from './routes/user';
+import vocaList from './routes/vocaList';
+import wordbook from './routes/wordbook';
 
 const api: FastifyPluginAsync = async (server: FastifyInstance) => {
   server.register(test, { prefix: '/' });
   server.register(user, { prefix: '/user' });
+  server.register(vocaList, { prefix: '/vocaList' });
+  server.register(wordbook, { prefix: '/wordbook' });
 };
 
 export default api;
