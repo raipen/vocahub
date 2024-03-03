47844994bbbbd72acf51deebed2f3e61d1c90063
raipen <raipen@knu.ac.kr>
2024-03-03 21:34:29 +0900

    feat: extract api Error Catch logic

--- src/front/utils/apis/auth.ts
+++ src/front/utils/apis/auth.ts
@@ -1,47 +1,21 @@
 import axios from 'axios';
 import * as User from '@DTO/user.dto';
-import { ErrorInterface } from '@DTO/index.dto';
-import { NetworkError,ErrorWithToast } from '@errors/index';
-import ErrorConfigs from '@errors/config';
+import { apiErrorCatchWrapper } from '..';
 type login = {
   name: string;
   password: string;
 }
 
-export const requestLogin = async ({name, password}: login) => {
-  try{
-    const response = await axios.post<User.signInInterface['Reply']['200']>('/api/v1/user/signIn', {name, password});
-    return response.data.accessToken;
-  }catch(e: unknown){
-    if(!axios.isAxiosError(e)||!e.response){
-      throw new NetworkError('NetworkError');
-    }
-    const {error:name, message} = e.response.data as ErrorInterface;
-    throw new ErrorConfigs[name].error(message);
-  }
-}
+export const requestLogin = apiErrorCatchWrapper(async ({name, password}: login)=>{
+  const response = await axios.post<User.signInInterface['Reply']['200']>('/api/v1/user/signIn', {name, password});
+  return response.data.accessToken;
+});
 
-export const requestRefresh = async () => {
-  try{
+export const requestRefresh = apiErrorCatchWrapper(async () => {
     const response = await axios.post<User.refreshInterface['Reply']['200']>('/api/v1/user/refresh');
     return response.data.accessToken;
-  }catch(e: unknown){
-    if(!axios.isAxiosError(e)||!e.response){
-      throw new NetworkError('NetworkError');
-    }
-    const {error:name, message} = e.response.data as ErrorInterface;
-    throw new ErrorConfigs[name].error(message);
-  }
-}
+});
 
-export const requestLogout = async () => {
-  try{
+export const requestLogout = apiErrorCatchWrapper(async () => {
     await axios.post('/api/v1/user/signOut');
-  }catch(e: unknown){
-    if(!axios.isAxiosError(e)||!e.response){
-      throw new NetworkError('NetworkError');
-    }
-    const {error:name, message} = e.response.data as ErrorInterface;
-    throw new ErrorConfigs[name].error(message);
-  }
-}
+});
--- src/front/utils/index.ts
+++ src/front/utils/index.ts
@@ -1,4 +1,21 @@
+import { ErrorInterface } from '@DTO/index.dto';
+import { NetworkError,ErrorWithToast } from '@errors/index';
+import ErrorConfigs from '@errors/config';
+import axios from 'axios';
+
 export function ISOStringToDateString(isoString: string) {
     const date = isoString.split('T')[0];
     return date.split('-').join('.');
+}
+
+export const apiErrorCatchWrapper = <T extends Array<any>,U>(api: (...a:T) => Promise<U>) => async (...a:T) => {
+    try {
+        return await api(...a);
+    } catch (e: unknown) {
+        if (!axios.isAxiosError(e) || !e.response) {
+            throw new NetworkError('NetworkError');
+        }
+        const { error: name, message } = e.response.data as ErrorInterface;
+        throw new ErrorConfigs[name].error(message);
+    }
 }
